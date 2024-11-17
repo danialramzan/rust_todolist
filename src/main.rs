f3ca9cfdf6a70fd760d0916ff96aba5ce0b6ff04
Danial Ramzan <danialramzan@gmail.com>
2024-11-17 04:55:41 -0800

    added stubs

--- src/main.rs
+++ src/main.rs
@@ -43,19 +43,12 @@ fn main() {
 
     // at this point we know that number is a valid input
 
-
     match number {
-        // Match a single value
-        1 => println!("One!"),
-        // Match several values
-        2 | 3 | 5 | 7 | 11 => println!("This is a prime"),
-        // TODO ^ Try adding 13 to the list of prime values
-        // Match an inclusive range
-        13..=19 => println!("A teen"),
-        // Handle the rest of cases
-        _ => println!("Ain't special"),
-        // TODO ^ Try commenting out this catch-all arm
+        // case statements
+        1 => wrapper.list(),
+        2 => wrapper.add(),
+        3 => wrapper.remove(),
+        4 => wrapper.edit(),
+        _ => {}
     }
-
-
 }
--- src/todo_list_wrapper.rs
+++ src/todo_list_wrapper.rs
@@ -1,9 +1,89 @@
 use super::todo_list::ToDoList;
+use std::io;
 
 pub struct ToDoListWrapper<'a> {
     todo_list: &'a mut ToDoList,
 }
 
 impl<'a> ToDoListWrapper<'a> {
-    // Methods for TodoListWrapper
+    pub fn new(todo_list: &'a mut ToDoList) -> Self {
+        ToDoListWrapper { todo_list }
+    }
+
+
+    // list function
+
+    pub fn list(&mut self) {
+        // let mut input = String::new();
+        // print!("Enter a description for the new task: ");
+        // io::stdout().flush().expect("Failed to flush output"); // Ensures the prompt is displayed before user input
+        //
+        // io::stdin()
+        //     .read_line(&mut input)
+        //     .expect("Failed to read input");
+        //
+        // let description = input.trim().to_string();
+        // if !description.is_empty() {
+        //     self.todo_list.add_task(description);
+        //     println!("Task added successfully!");
+        // } else {
+        //     println!("Task description cannot be empty.");
+        // }
+    }
+    pub fn add(&mut self) {
+        // let mut input = String::new();
+        // print!("Enter a description for the new task: ");
+        // io::stdout().flush().expect("Failed to flush output"); // Ensures the prompt is displayed before user input
+        //
+        // io::stdin()
+        //     .read_line(&mut input)
+        //     .expect("Failed to read input");
+        //
+        // let description = input.trim().to_string();
+        // if !description.is_empty() {
+        //     self.todo_list.add_task(description);
+        //     println!("Task added successfully!");
+        // } else {
+        //     println!("Task description cannot be empty.");
+        // }
+    }
+
+    pub fn remove(&mut self) {
+        // let mut input = String::new();
+        // print!("Enter a description for the new task: ");
+        // io::stdout().flush().expect("Failed to flush output"); // Ensures the prompt is displayed before user input
+        //
+        // io::stdin()
+        //     .read_line(&mut input)
+        //     .expect("Failed to read input");
+        //
+        // let description = input.trim().to_string();
+        // if !description.is_empty() {
+        //     self.todo_list.add_task(description);
+        //     println!("Task added successfully!");
+        // } else {
+        //     println!("Task description cannot be empty.");
+        // }
+    }
+
+    pub fn edit(&mut self) {
+        // let mut input = String::new();
+        // print!("Enter a description for the new task: ");
+        // io::stdout().flush().expect("Failed to flush output"); // Ensures the prompt is displayed before user input
+        //
+        // io::stdin()
+        //     .read_line(&mut input)
+        //     .expect("Failed to read input");
+        //
+        // let description = input.trim().to_string();
+        // if !description.is_empty() {
+        //     self.todo_list.add_task(description);
+        //     println!("Task added successfully!");
+        // } else {
+        //     println!("Task description cannot be empty.");
+        // }
+    }
+
+
+    // Methods for TodoListWrapper can go here
 }
